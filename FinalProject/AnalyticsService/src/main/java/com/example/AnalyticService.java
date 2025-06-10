58dd13fdd45cbd57d7d39ea6eab09c5a91e287d4
RFRsk <rfrsk@yandex.ru>
2025-06-10 12:33:50 +0300

    Чтение запросов и запись в хадуп

---
+++ FinalProject/AnalyticsService/src/main/java/com/example/AnalyticService.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import com.examples.kafka.RequestsConsumer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@SpringBootApplication()
+@ComponentScan("com.examples.kafka")
+public class AnalyticService {
+
+    @Autowired
+    RequestsConsumer requestsConsumer;
+
+    public static void main( String[] args )
+    {
+        SpringApplication.run(AnalyticService.class, args);
+    }
+}
